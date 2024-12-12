ce182a3a12676cfd8bc7640ec1face240803f309
Guzel <nabiullinaguzel93@gmail.com>
2024-12-12 09:25:13 +0300

    index page

---
+++ js/main.js
@@ -0,0 +1,29 @@
+function openMenu(id){
+    document.getElementById(id).classList.add('visible');
+}
+function closeMenu(id){
+    document.getElementById(id).classList.remove('visible');
+}
+
+// document.addEventListener("load", ready);
+// function ready() {
+//     let iframeArr = Array.from(document.querySelectorAll('.news-list iframe'));
+//     console.log(iframeArr.length);
+//     iframeArr.forEach(function(item) {
+//         item.contentWindow.document.head.insertAdjacentHTML(
+//             'beforeend',
+//             '<style>body { background-color: red; }</style>'
+//           );
+//     });
+//   }
+window.onload = () => {
+    let iframeArr = Array.from(document.querySelectorAll('.news-list iframe'));
+    console.log(iframeArr.length);
+    iframeArr.forEach((item) => {
+        item.contentWindow.document.head.insertAdjacentHTML(
+            'beforeend',
+            '<style>body { background-color: red; }</style>'
+          );
+    });
+    
+}
